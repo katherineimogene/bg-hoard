46de2fa76a7d5d9f8a74cfd679c14489ec7c7316
Katie Hayden <katherineimogene@gmail.com>
2021-07-06 16:02:23 -0700

    lab8 routed game-detail component

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
+// import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,7 +10,9 @@ import { formatRating } from '@bg-hoard/store/util-formatters';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
+
   title = 'Board Game Hoard';
-  games = getAllGames();
   formatRating = formatRating;
+  games = this.http.get<any[]>('/api/games');
 }
