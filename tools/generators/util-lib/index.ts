007f1374d3ba431e3c99c40c3684f49a7bfc2157
Katie Hayden <katherineimogene@gmail.com>
2021-07-07 09:38:10 -0700

    finish lab 13

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -4,7 +4,8 @@ import { libraryGenerator } from '@nrwl/workspace/generators';
 export default async function (host: Tree, schema: any) {
   await libraryGenerator(host, {
     name: `util-${schema.name}`, 
-    directory: schema.directory
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`
   });
   await formatFiles(host);
   return () => {
